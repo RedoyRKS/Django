3230b566bd5b6f0885d62bea18635b57eee70eba
RedoyRKS <redoyshaha06@gmail.com>
2023-08-09 23:34:11 +0600

    learn6

--- core/home/models.py
+++ core/home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    address = models.TextField(null=True , blank=True)
+    email = models.EmailField(null=True , blank=True)
+    
+
+class Product(models.Model):
+    pName = models.CharField(max_length=50)
+    pDate = models.DateField()    
