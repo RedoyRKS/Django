f26a1f6465070375e92b8d2b33b1b8405c289b6a
RedoyRKS <redoyshaha06@gmail.com>
2023-08-15 23:57:20 +0600

    class8

--- core/home/models.py
+++ core/home/models.py
@@ -2,13 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+
 class Student(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
-    address = models.TextField(null=True , blank=True)
-    email = models.EmailField(null=True , blank=True)
-    
+    address = models.TextField(null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
 
-class Product(models.Model):
-    pName = models.CharField(max_length=50)
-    pDate = models.DateField()    
+
+class Car(models.Model):
+    car_name = models.CharField(max_length=50)
+    car_speed = models.IntegerField(default=50)
+
+    def __str__(self) -> str:
+        return self.car_name
+    
+    
